575fcd7438ac993b0234013d438f9e24d8889c98
devSominPark <min85472@gmail.com>
2021-12-02 17:41:31 +0900

    UPDATE Home_modified css & added department section

--- components/home/SectionTitle.tsx
+++ components/home/SectionTitle.tsx
@@ -6,8 +6,8 @@ interface IProps {
 const SectionTitle = ({ title, firstSubTitle, secondSubTitle }: IProps) => {
   return (
     <>
-      <section className="lg:mt-20 md:mt-20 mt-16">
-        <header className="font-main font-bold lg:text-3xl md:text-2xl text-xl lg:mb-2 text-black-main">
+      <section className="lg:mt-20 md:mt-12 mt-8">
+        <header className="font-main font-bold lg:text-3xl md:text-2xl text-lg lg:mb-2 text-black-main">
           {title}
         </header>
         <hgroup className="flex lg:flex-row md:flex-row flex-col font-main font-nomal lg:text-xl md:text-lg text-sm text-gray-sub">
--- pages/department/[name].tsx
+++ pages/department/[name].tsx
@@ -1,13 +1,17 @@
 import HeadInfo from "../../components/global/HeadInfo";
+import { useRouter } from "next/router";
 
 const Department = () => {
+  const router = useRouter();
+  const { name } = router.query;
+
   return (
     <>
       <HeadInfo
         title={"진료과별 이야기"}
         content={"진료과별 이야기를 함께 나눠 보세요."}
       />
-      <div>Department</div>
+      <div>Department : {name}</div>
     </>
   );
 };
