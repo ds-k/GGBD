f66b45320c4410fc1a1e0b451a701a1f34d2f897
devSominPark <min85472@gmail.com>
2021-12-04 17:02:01 +0900

    CREATE Department_connecting data to each page

--- pages/department/[name].tsx
+++ pages/department/[name].tsx
@@ -1,9 +1,8 @@
+import axios from "axios";
 import HeadInfo from "../../components/global/HeadInfo";
-import { useRouter } from "next/router";
 
-const Department = () => {
-  const router = useRouter();
-  const { name } = router.query;
+const Department = ({ department }: any) => {
+  const { name, description } = department;
 
   return (
     <>
@@ -12,8 +11,42 @@ const Department = () => {
         content={"진료과별 이야기를 함께 나눠 보세요."}
       />
       <div>Department : {name}</div>
+      <div>{description}</div>
     </>
   );
 };
 
 export default Department;
+
+interface IProps {
+  id: number;
+  name: string;
+}
+
+export async function getStaticPaths() {
+  const getData = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/department`
+  );
+  const data = getData.data;
+
+  return {
+    paths: data.map((el: IProps) => ({
+      params: {
+        name: el.name.toString(),
+      },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: any) {
+  const getData = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/department/${encodeURI(params.name)}`
+  );
+  const data = getData.data;
+  return {
+    props: {
+      department: data,
+    },
+  };
+}
