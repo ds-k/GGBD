22947031a27d25e484a58c0ce656b5e833bc936a
devSominPark <min85472@gmail.com>
2021-12-12 03:30:29 +0900

    CREATE Hospital_layout

--- components/hospital/RegionSection.tsx
+++ components/hospital/RegionSection.tsx
@@ -1,10 +1,48 @@
+import { useState } from "react";
 import ListTitle from "../common/ListTitle";
 
 const RegionSection = () => {
+  const [region, setRegion] = useState<string>("전체");
+  const regions = [
+    "전체",
+    "서울",
+    "경기 남부",
+    "경기 서북부",
+    "강원",
+    "충남",
+    "충북",
+    "전남",
+    "전북",
+    "경북",
+    "경남 동부",
+    "경남 서부",
+  ];
+
   return (
-    <section>
-      <ListTitle title={"지역"} />
-    </section>
+    <div className="flex flex-col">
+      {/* List Title */}
+      <section>
+        <ListTitle title={"지역"} />
+      </section>
+      {/* Region List */}
+      <section className="grid lg:grid-cols-2 grid-cols-4 gap-x-8 gap-y-0 my-5 ml-2">
+        {regions.map((el, idx) => {
+          return (
+            <div key={idx}>
+              <div
+                className={
+                  "cursor-pointer my-1 font-sub font-normal lg:text-lg text-base hover:text-blue-main active:text-blue-main " +
+                  (el === region ? "text-blue-main" : "text-gray-sub")
+                }
+                onClick={() => setRegion(el)}
+              >
+                {el}
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </div>
   );
 };
 
--- pages/hospital/index.tsx
+++ pages/hospital/index.tsx
@@ -1,10 +1,24 @@
+import axios from "axios";
 import HeadInfo from "../../components/global/HeadInfo";
 import Title from "../../components/common/Title";
 import Image from "next/image";
 import HospitalSection from "../../components/hospital/HospitalSection";
 import RegionSection from "../../components/hospital/RegionSection";
 
-const Hospital = () => {
+interface IProps {
+  hospitals: [
+    {
+      id: number;
+      region: string;
+      name: string;
+      homepage: string;
+      phone: string;
+      address: string;
+    }
+  ];
+}
+
+const Hospital = ({ hospitals }: IProps) => {
   return (
     <>
       {/* headInfo */}
@@ -13,7 +27,7 @@ const Hospital = () => {
         content={"보건복지부에서 선정한 전국 45개의 상급종합병원 목록입니다."}
       />
       {/* Hospital Page */}
-      <div className="flex justify-center md:p-8 p-4 h-screen">
+      <div className="flex justify-center md:p-8 p-4">
         <div className="lg:w-lg w-screen">
           {/* Title Section */}
           <Title
@@ -25,16 +39,16 @@ const Hospital = () => {
           {/* Img Section */}
           <section className="lg:my-4 md:my-3 my-2 w-full">
             <Image
-              src="/images/hospital/gate_picture.svg"
-              alt="gate_picture"
+              src="/images/hospital/gate_Picture.svg"
+              alt="gate_Picture"
               width={1031}
               height={277}
             />
           </section>
           {/* contents */}
-          <div className="flex justify-between">
+          <div className="flex lg:flex-row lg:justify-between flex-col-reverse">
             {/* Hospital List Section */}
-            <HospitalSection />
+            <HospitalSection hospitals={hospitals} />
             {/* Region List Section */}
             <RegionSection />
           </div>
@@ -45,3 +59,15 @@ const Hospital = () => {
 };
 
 export default Hospital;
+
+export async function getStaticProps() {
+  const getData = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/Hospital`
+  );
+  const data = getData.data;
+  return {
+    props: {
+      hospitals: data,
+    },
+  };
+}
