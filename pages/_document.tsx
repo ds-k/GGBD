a7db4c8fa2fb1ada6d806a3a5de3e7b0db5edb66
daeseongkim05 <daeseongkim05@gmail.com>
2021-12-28 21:50:44 +0900

    ROLLBACK font-setting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Gowun+Batang:wght@400;700&family=Noto+Sans+KR&family=Roboto&display=swap"
           rel="stylesheet"
-        /> 
+        />
       </Head>
       <body>
         <Main />
