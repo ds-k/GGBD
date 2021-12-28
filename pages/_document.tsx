b41dc73074eebb68cdf523b3157336fa746b7711
daeseongkim05 <daeseongkim05@gmail.com>
2021-12-28 21:46:31 +0900

    ROLLBACK font-setting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Gowun+Batang:wght@400;700&family=Noto+Sans+KR&family=Roboto&display=swap"
           rel="stylesheet"
-        /> */}
+        />
       </Head>
       <body>
         <Main />
